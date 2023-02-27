cd2d21b231958c7c4b5a63e2e06d4a171fcc5306
bhupathirayudu <100065554+bhupathirayudu@users.noreply.github.com>
2023-02-27 14:17:14 +0530

    Add files via upload

---
+++ course_status/lang/en/block_course_status.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Languages configuration for the block_pluginname plugin.
+ *
+ * @package   block_course_status
+ * @copyright 2023, bhupathi
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Course Status block';
+$string['pluginname'] = 'Course Status';
+$string['course_status:addinstance'] = 'Add a new course_status block';
+$string['course_status:myaddinstance'] = 'Add a new course_status block to the My Moodle page';
+$string['blockstring'] = 'Course Status String';
+$string['blocksettings'] = 'Course Status Block Settings';
+
+$string['complete'] = 'Complete';
+$string['blocktitle'] = 'Course Status';
+$string['defaulttitle'] = 'course status';
+$string['defaulttext'] = 'Course Status View';
